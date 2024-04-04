2a2f7414df19cc286334dbad9fd0fcb72837153d
David LiaBraaten <davidliabraaten@gmail.com>
2024-04-04 10:10:38 -0700

    react-portfolio initialize create-react-app typescript mui

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          David LiaBraaten | Web Developer
+        </p>
+        <Button variant="contained">Hello world</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
